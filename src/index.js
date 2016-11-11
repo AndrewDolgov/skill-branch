d4a131c0c9a89db63ba9d61f8dd60006b994c998
Andrew Dolgov <doland@yandex.ru>
2016-11-12 00:49:40 +0400

    task2b

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
-var express = require('express');
-var cors =  require( 'cors');
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
@@ -11,8 +11,36 @@ app.get('/', (req, res) => {
 });
 
 app.get('/task2a', (req, res) => {
-  const sum = (+req.query.a||0) +(+req.query.b||0);
+  const sum = (+req.query.a || 0) + (+req.query.b || 0);
   res.send(sum.toString());
+});
+
+const getInitials = (person) => {
+
+  if (!person) {
+    return 'Invalid fullname';
+  }
+
+  if (/[\d_/]/.test(person)) {
+    return 'Invalid fullname';
+  }
+
+  var parts = person.split(' ');
+  if (parts.length < 1 || parts.length > 3) {
+    return 'Invalid fullname';
+  }
+
+  var initials = [parts.pop()];
+  parts.forEach(function (item, i) {
+    parts[i] = item[0] + '.';
+  });
+
+  return initials.concat(parts).join(' ');
+};
+
+app.get('/task2b', (req, res) => {
+  const result = getInitials(req.query.fullname.toString());
+  res.send(result);
 });
 
 app.listen(3000, () => {
